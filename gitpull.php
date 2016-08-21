162b35fcc02a2fffb6f168e7685468b434434438
king <phoenix_jinqi@163.com>
2016-08-21 16:29:47 +0800

    git pull

---
+++ gitpull.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <meta http-equiv="Expires" content="0">
+
+    <title>pull new version</title>
+</head>
+<body>
+
+<b>pull code</b>
+<br />
+<?php
+passthru("git pull");
+?>
+</body>
+</html>
